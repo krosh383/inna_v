30c54e85ef83103e211ee261ece5b5167406fc55
krosh383 <142114757+krosh383@users.noreply.github.com>
2023-08-13 00:29:55 +0500

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://9401333c-b6e8-4aeb-a1e5-44a48210fe86.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Инна",
+    "lastName": "Воробьева",
+    "address": "Гоголя 79",
+    "metroStation": 99,
+    "phone": "+7123456789",
+    "rentTime": 4,
+    "deliveryDate": "2023-08-12",
+    "comment": "не звониить - злая собака",
+    "color": [
+        "BLACK"
+    ]
+}
